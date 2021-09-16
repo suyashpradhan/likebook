fa38a1c616489f6c32c6f9bd8983ef41cd39e6f9
suyashpradhan <pradhansuyash39@gmail.com>
2021-09-16 20:01:58 +0530

    feat: ready for deployment

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRY: process.env.JWT_EXPIRY,
     JWT_COOKIE_EXPIRY: process.env.JWT_COOKIE_EXPIRY,
-    API_URL:
+    PRODUCTION_URL:
       process.env.NODE_ENV === "development"
         ? "http://localhost:3000"
         : "https://likebook.vercel.app",
